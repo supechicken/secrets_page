3ceaa6308a2664643ffe040707e15166e958c48d
SupeChicken666 <me@supechicken666.dev>
2025-11-04 02:10:43 +0800

    tr

---
+++ automate.rb
@@ -0,0 +1,27 @@
+def input_tap(x, y)
+  system "adb -s localhost:5555 shell input_tap #{x} #{y}"
+  sleep 0.5
+end
+
+
+Dir.chdir '/home/supechicken/secrets_page' do
+  current_head = `git rev-parse --short HEAD`
+
+  system 'git pull'
+
+  if current_head != `git rev-parse --short HEAD`
+    # have update
+
+    input_tap 153, 322
+    input_tap 343, 573
+    input_tap 255, 90
+
+    system "adb shell am broadcast -a ADB_INPUT_TEXT --es msg 'test'"
+    sleep 0.5
+
+    input_tap 212, 1033
+    input_tap 186, 897
+    input_tap 624, 66
+    input_tap 650, 1118
+  end
+end
