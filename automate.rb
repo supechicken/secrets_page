554d6ebcb981e0051eb1b32a9331e831a34e20d2
SupeChicken666 <me@supechicken666.dev>
2025-11-04 02:48:56 +0800

    test

--- automate.rb
+++ automate.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'open3'
 
 def input_tap(x, y)
   system "adb -s localhost:5555 shell input swipe #{x} #{y} #{x} #{y} 150"
@@ -22,7 +23,7 @@ Dir.chdir '/home/supechicken/secrets_page' do
     input_tap 343, 573
     input_tap 255, 90
 
-    system 'adb', 'shell', 'am', 'broadcast', '-a', 'ADB_INPUT_TEXT', '--es', 'msg', response[:response].inspect
+    Open3.capture2 "adb shell xargs -d '\0' am broadcast -a ADB_INPUT_TEXT --es msg", stdin_data: response[:response]
     sleep 0.5
 
     input_tap 212, 1033
