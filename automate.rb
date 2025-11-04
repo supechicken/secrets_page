e9e634d53c0e10a80b2aa2c5ac600c3f27ab4359
SupeChicken666 <me@supechicken666.dev>
2025-11-04 16:04:39 +0800

    onsync

--- automate.rb
+++ automate.rb
@@ -13,18 +13,19 @@ Dir.chdir '/home/supechicken/secrets_page' do
   system 'adb connect localhost:5555'
 
   updated_head      = `git rev-parse --short HEAD`
-  last_changed_file = `git diff --name-status HEAD~ HEAD | grep '^A'`.chomp.split("\t", 2)[-1]
+  last_changed_file = `git diff --name-status HEAD~ HEAD | grep '^A'`.lines(chomp: true).map { |l| l.split("\t", 2)[-1] }
 
-  if last_changed_file && (ARGV.include?('--force') || current_head != updated_head)
+  if last_changed_file.any? && (ARGV.include?('--force') || current_head != updated_head)
     response = JSON.load_file(last_changed_file, symbolize_names: true)
 
-    # have update
-    input_tap 150, 312
-    input_tap 343, 573
-    input_tap 255, 90
+    last_changed_file.each do |f|
+      # have update
+      input_tap 150, 312
+      input_tap 343, 573
+      input_tap 255, 90
 
-    Open3.capture2 "adb shell xargs -0 am broadcast -a ADB_INPUT_TEXT --es msg", stdin_data: response[:response]
-    sleep 0.5
+      Open3.capture2 "adb shell xargs -0 am broadcast -a ADB_INPUT_TEXT --es msg", stdin_data: response[:response]
+      sleep 0.5
 
     input_tap 212, 1033
     input_tap 186, 897
